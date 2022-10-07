7c86ccf6ffbf81d9765679378fe529e4ba2ed152
Wanbo.Wang <wanbo.wang@georgebrown.ca>
2022-10-07 16:52:22 -0400

    added bulletBehaviour

---
+++ Assets/[Scripts]/BulletBehaviour.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+[System.Serializable]
+public struct ScreenBounds
+{
+    public Boundary horizontal;
+    public Boundary vertical;
+}
+
+public class BulletBehaviour : MonoBehaviour
+{
+    public BulletDirection direction;
+    public float speed;
+    public Vector3 velocity;
+    public ScreenBounds bounds;
+
+    void Start()
+    {
+        switch (direction)
+        {
+            case BulletDirection.UP:
+                velocity = Vector3.up * speed;
+                break;
+            case BulletDirection.RIGHT:
+                velocity = Vector3.right * speed;
+                break;
+            case BulletDirection.DOWN:
+                velocity = Vector3.down * speed;
+                break;
+            case BulletDirection.LEFT:
+                velocity = Vector3.left * speed;
+                break;
+        }
+    }
+
+    void Update()
+    {
+        Move();
+        CheckBounds();
+    }
+
+    void Move()
+    {
+        transform.position += velocity * Time.deltaTime;
+    }
+
+    void CheckBounds()
+    {
+        if ((transform.position.x > bounds.horizontal.max) ||
+            (transform.position.x < bounds.horizontal.min) ||
+            (transform.position.y > bounds.vertical.max) ||
+            (transform.position.y < bounds.vertical.min))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(this.gameObject);
+    }
+
+}
